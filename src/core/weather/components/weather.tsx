cf1d3d841a330f5afd6d0fb0f58ffcffa2d70e2a
b6i0l5l <b6i0l5l@mail.fresnosate.edu>
2021-11-02 23:11:28 -0500

    add display flex to align the center

--- src/core/weather/components/weather.tsx
+++ src/core/weather/components/weather.tsx
@@ -7,15 +7,24 @@ export const Weather: FC = () => {
   const { weatherData, getWeather } = useGetWeather();
 
   return (
-    <>
+    <div
+      style={{
+        height: '500px',
+        justifyContent: 'center',
+        justifyItems: 'center',
+        alignItems: 'center',
+        alignContent: 'center',
+        display: 'flex',
+      }}
+    >
       <Row justify="center">
-        <Col span={8}>The temperature is {weatherData.main?.temp}</Col>
+        <Col>The temperature is {weatherData.main?.temp}</Col>
       </Row>
       <Row justify="center">
-        <Col span={8}>
+        <Col>
           <Input placeholder="input search text" onPressEnter={getWeather} />
         </Col>
       </Row>
-    </>
+    </div>
   );
 };
