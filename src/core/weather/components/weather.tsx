a6d960221c9c23e1e8c9c6acefac9ec415ccf98b
b6i0l5l <b6i0l5l@mail.fresnosate.edu>
2021-11-04 22:24:03 -0500

    add switch temperature format

--- src/core/weather/components/weather.tsx
+++ src/core/weather/components/weather.tsx
@@ -1,30 +1,48 @@
 import { FC } from 'react';
-import { Row, Col, Input } from 'antd';
+import { Row, Col, Input, Layout, Typography } from 'antd';
 import 'antd/dist/antd.css';
 import { useGetWeather } from '../hooks/get';
 
+const { Content } = Layout;
+const { Text } = Typography;
+
 export const Weather: FC = () => {
-  const { weatherData, getWeather } = useGetWeather();
+  const { temperature, getWeather, setTemFormat } = useGetWeather();
 
   return (
-    <div
-      style={{
-        height: '500px',
-        justifyContent: 'center',
-        justifyItems: 'center',
-        alignItems: 'center',
-        alignContent: 'center',
-        display: 'flex',
-      }}
-    >
-      <Row justify="center">
-        <Col>The temperature is {weatherData.main?.temp}</Col>
-      </Row>
-      <Row justify="center">
-        <Col>
-          <Input placeholder="input search text" onPressEnter={getWeather} />
-        </Col>
-      </Row>
-    </div>
+    <Layout style={{ height: '100%', display: 'flex' }}>
+      <Content
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Row justify="center">
+          <Col style={{ fontSize: '36px' }}>
+            {temperature ? temperature : '--'}{' '}
+            <Text
+              style={{ cursor: 'pointer' }}
+              onClick={() => setTemFormat(true)}
+            >
+              <sup>o</sup>F
+            </Text>
+            <Text>/</Text>
+            <Text
+              style={{ cursor: 'pointer' }}
+              onClick={() => setTemFormat(false)}
+            >
+              <sup>o</sup>C
+            </Text>
+          </Col>
+        </Row>
+        <Row justify="center">
+          <Col>
+            <Input placeholder="Enter a city..." onPressEnter={getWeather} />
+          </Col>
+        </Row>
+      </Content>
+    </Layout>
   );
 };
--- src/core/weather/hooks/get.ts
+++ src/core/weather/hooks/get.ts
@@ -1,17 +1,18 @@
-import { useState, KeyboardEvent } from 'react';
+import { useState, KeyboardEvent, Dispatch, SetStateAction } from 'react';
 import useFetch from 'use-http';
 import { GET_WEATHER_API } from '../api/getWeather';
 
 interface UseGetWeatherReturn {
-  weatherData: any;
   getWeather: (e: KeyboardEvent) => Promise<any>;
-  fahrenheitToCelcius: (f: number) => string;
+  setTemFormat: Dispatch<SetStateAction<boolean>>;
+  temperature: string;
 }
 
 export const useGetWeather = (): UseGetWeatherReturn => {
   const { get, response } = useFetch(GET_WEATHER_API);
 
-  const [weatherData, setWeatherData] = useState({});
+  const [weatherData, setWeatherData] = useState(null);
+  const [tempFormat, setTemFormat] = useState(true);
 
   const getWeather = async (e: KeyboardEvent): Promise<any> => {
     const city = (e.target as HTMLInputElement).value;
@@ -24,12 +25,14 @@ export const useGetWeather = (): UseGetWeatherReturn => {
   };
 
   const fahrenheitToCelcius = (f: number): string => {
-    return (((f - 32) * 6) / 9).toString();
+    return (((f - 32) * 6) / 9).toFixed(1).toString();
   };
 
   return {
-    weatherData,
+    temperature: tempFormat
+      ? weatherData?.main.temp.toFixed(1)
+      : fahrenheitToCelcius(weatherData?.main.temp),
     getWeather,
-    fahrenheitToCelcius,
+    setTemFormat,
   };
 };
